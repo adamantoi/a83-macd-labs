02bf6f2f41b76110e3621152c0a0c2f10e0e176f
beny <adamantoi@yahoo.com>
2019-04-05 10:06:12 +0700

    Update index.php
    set username dll

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dbdicoding.database.windows.net";
+    $user = "adamantoi";
+    $pass = "Asdf.1234";
+    $db = "dbDicoding1";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
